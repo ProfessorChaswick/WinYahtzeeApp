b236201257bb4a8cb9520a40b65a52b809ea1247
ProfessorChaswick <112112334+ProfessorChaswick@users.noreply.github.com>
2022-09-01 19:36:22 -0400

    Continued with Picture Boxes

--- WinYahtzee/Form1.Designer.cs
+++ WinYahtzee/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnRoll = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.picDi5 = new System.Windows.Forms.PictureBox();
@@ -35,6 +36,7 @@
             this.picDi2 = new System.Windows.Forms.PictureBox();
             this.picDi3 = new System.Windows.Forms.PictureBox();
             this.picDi4 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.picDi5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picDi1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picDi2)).BeginInit();
@@ -44,7 +46,7 @@
             // 
             // btnRoll
             // 
-            this.btnRoll.Location = new System.Drawing.Point(565, 546);
+            this.btnRoll.Location = new System.Drawing.Point(478, 528);
             this.btnRoll.Name = "btnRoll";
             this.btnRoll.Size = new System.Drawing.Size(75, 23);
             this.btnRoll.TabIndex = 0;
@@ -55,7 +57,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(669, 9);
+            this.label1.Location = new System.Drawing.Point(553, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 15);
             this.label1.TabIndex = 1;
@@ -63,46 +65,56 @@
             // 
             // picDi5
             // 
+            this.picDi5.BackColor = System.Drawing.Color.Green;
             this.picDi5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.picDi5.Location = new System.Drawing.Point(663, 505);
+            this.picDi5.Image = global::WinYahtzee.Resource1.blank;
+            this.picDi5.Location = new System.Drawing.Point(576, 487);
             this.picDi5.Name = "picDi5";
-            this.picDi5.Size = new System.Drawing.Size(35, 35);
+            this.picDi5.Size = new System.Drawing.Size(35, 40);
             this.picDi5.TabIndex = 2;
             this.picDi5.TabStop = false;
             // 
             // picDi1
             // 
+            this.picDi1.BackColor = System.Drawing.Color.Green;
             this.picDi1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.picDi1.Location = new System.Drawing.Point(499, 505);
+            this.picDi1.Image = global::WinYahtzee.Resource1.blank;
+            this.picDi1.Location = new System.Drawing.Point(412, 487);
             this.picDi1.Name = "picDi1";
-            this.picDi1.Size = new System.Drawing.Size(35, 35);
+            this.picDi1.Size = new System.Drawing.Size(35, 40);
             this.picDi1.TabIndex = 3;
             this.picDi1.TabStop = false;
             // 
             // picDi2
             // 
+            this.picDi2.BackColor = System.Drawing.Color.Green;
             this.picDi2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.picDi2.Location = new System.Drawing.Point(540, 505);
+            this.picDi2.Image = global::WinYahtzee.Resource1.blank;
+            this.picDi2.Location = new System.Drawing.Point(453, 487);
             this.picDi2.Name = "picDi2";
-            this.picDi2.Size = new System.Drawing.Size(35, 35);
+            this.picDi2.Size = new System.Drawing.Size(35, 40);
             this.picDi2.TabIndex = 4;
             this.picDi2.TabStop = false;
             // 
             // picDi3
             // 
+            this.picDi3.BackColor = System.Drawing.Color.Green;
             this.picDi3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.picDi3.Location = new System.Drawing.Point(581, 505);
+            this.picDi3.Image = global::WinYahtzee.Resource1.blank;
+            this.picDi3.Location = new System.Drawing.Point(494, 487);
             this.picDi3.Name = "picDi3";
-            this.picDi3.Size = new System.Drawing.Size(35, 35);
+            this.picDi3.Size = new System.Drawing.Size(35, 40);
             this.picDi3.TabIndex = 5;
             this.picDi3.TabStop = false;
             // 
             // picDi4
             // 
+            this.picDi4.BackColor = System.Drawing.Color.Green;
             this.picDi4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.picDi4.Location = new System.Drawing.Point(622, 505);
+            this.picDi4.Image = global::WinYahtzee.Resource1.blank;
+            this.picDi4.Location = new System.Drawing.Point(535, 487);
             this.picDi4.Name = "picDi4";
-            this.picDi4.Size = new System.Drawing.Size(35, 35);
+            this.picDi4.Size = new System.Drawing.Size(35, 40);
             this.picDi4.TabIndex = 6;
             this.picDi4.TabStop = false;
             // 
@@ -110,7 +122,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(707, 569);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(617, 569);
             this.Controls.Add(this.picDi4);
             this.Controls.Add(this.picDi3);
             this.Controls.Add(this.picDi2);
@@ -139,5 +152,6 @@
         private PictureBox picDi2;
         private PictureBox picDi3;
         private PictureBox picDi4;
+        private ToolTip toolTip1;
     }
 }
--- WinYahtzee/Form1.cs
+++ WinYahtzee/Form1.cs
@@ -9,13 +9,14 @@ namespace WinYahtzee
         public Form1()
         {
             InitializeComponent();
+            toolTip1.SetToolTip(picDi1, "Click to hold");
         }
 
         private async void btnRoll_Click(object sender, EventArgs e)
         {
            label1.Text = Roll.rollEm().ToString();
             picDi1.Image = Resource1.smDiceroll;
-            await Task.Delay(2000);
+            await Task.Delay(1500);
             picDi1.Refresh();
             picDi1.Image = Resource1.y4;
         }
